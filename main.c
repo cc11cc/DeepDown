fc445a1dda7327db7b6076a80ee900ff97a5f911
dp0304 <dp0304@gmail.com>
2012-07-20 16:28:33 +0800

    刚开始,用netbeans, 用libev,用curses...

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: dp
+ *
+ * Created on 2012年7月20日, 下午4:22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ tcp_server.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   tcp_server.h
+ * Author: dp
+ *
+ * Created on 2012年7月20日, 下午4:26
+ */
+
+#ifndef TCP_SERVER_H
+#define	TCP_SERVER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TCP_SERVER_H */
+
